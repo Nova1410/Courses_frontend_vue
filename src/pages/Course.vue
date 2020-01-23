3ba9174d86589b583160dd0a621756b5499feca6
Juan Cortés <jecortes27@misena.edu.com>
2020-01-22 23:12:45 -0500

    Page for the Courses

---
+++ src/pages/Course.vue
@@ -0,0 +1,29 @@
+<template>
+<q-page padding>
+  <div class="doc-container">
+    <div class="row">
+      <div class="col-6">
+        <q-input class="q-mr-md" v-model="search_term" label="Search all courses">
+          <template v-slot:prepend>
+            <q-icon color="blue-5" name="search" />
+          </template>
+        </q-input>
+      </div>
+    </div>
+  </div>
+</q-page>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        search_term: ''
+      }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
