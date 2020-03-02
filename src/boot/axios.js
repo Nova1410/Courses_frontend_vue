cb2e20a3b1e2da02f0971d22c819af8beb66ba57
Juan Cortés <jecortes27@misena.edu.com>
2020-03-01 19:54:27 -0500

    Add function to axios to intercept each request and add the authorization token to the headers

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -8,6 +8,17 @@ const axiosInstace = axios.create({
     baseURL: 'http://localhost:8000/api/'
   })
 
+// Intercept request to send the authorization in the headers
+axiosInstace.interceptors.request.use(config => {
+  let token = localStorage.getItem('token')
+  if(token !== null){
+    config['headers']['Authorization'] = 'Bearer ' + token
+  }
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
 //Export Axios like an instance of Vue
   export default ({ Vue }) => {
     Vue.prototype.$axios = axiosInstace
