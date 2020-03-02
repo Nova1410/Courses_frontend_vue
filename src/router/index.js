00c6e3681d8a70a6dc1dea62dd8b81a5dc2bdcc3
Juan Cortés <jecortes27@misena.edu.com>
2020-03-01 19:59:32 -0500

    Add function to verify if the user is authenticated when the route need authentication

--- src/router/index.js
+++ src/router/index.js
@@ -2,29 +2,35 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import routes from './routes'
+import libs from '../libs/libs'
 
 Vue.use(VueRouter)
 
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Router instance.
- */
 
-export default function (/* { store, ssrContext } */) {
-  const Router = new VueRouter({
-    scrollBehavior: () => ({ x: 0, y: 0 }),
-    routes,
 
-    // Leave these as they are and change in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    mode: process.env.VUE_ROUTER_MODE,
-    base: process.env.VUE_ROUTER_BASE
-  })
+const Router = new VueRouter({
+  scrollBehavior: () => ({ x: 0, y: 0 }),
+  routes,
+  mode: process.env.VUE_ROUTER_MODE,
+  base: process.env.VUE_ROUTER_BASE
+})
 
-  return Router
-}
+Router.beforeEach((to, from, next) => {
+  let nextObject = {}
+
+  // Search if the route requires Authentication
+  if(to.matched.some( record => record.meta.requiresAuth)) {
+    // Verify that the user is authenticated
+    if (!libs.isAuthenticated()) {
+      nextObject = {
+        path: '/',
+        query: { redirect: to.fullPath }
+      }
+      next(nextObject)
+    }
+  }
+  next(nextObject)
+})
+
+  export default Router
+
