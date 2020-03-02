cc5f35de68b83b8e53d553097cd698dac301eb52
Juan Cortés <jecortes27@misena.edu.com>
2020-03-01 19:55:38 -0500

    Add a menu with a button to log out

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -8,6 +8,18 @@
           </q-avatar>
           Course Catalog
         </q-toolbar-title>
+        <q-btn flat dense round>
+          <q-icon name="more_vert" />
+          <q-menu>
+            <q-list link separator class="scroll" style="min-width: 100px">
+              <q-item clickable @click.native="logOut()" style="padding: none; ">
+                <q-item-section>
+                  Log Out
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
       </q-toolbar>
     </q-header>
 
@@ -18,12 +30,19 @@
 </template>
 
 <script>
+import libs from '../libs/libs'
+
 export default {
   name: 'MyLayout',
 
   data () {
     return {
       
+    }
+  },
+  methods:{
+    logOut() {
+      libs.logOut()
     }
   }
 }
